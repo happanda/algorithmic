7486d2e9973450cf74ebe08217b018e653703574
Pavel Moskalevich <happy.panda.code@gmail.com>
2015-09-07 16:25:16 +0300

    fixed node hierarchy

--- src/skip_list.h
+++ src/skip_list.h
@@ -1,12 +1,32 @@
 #ifndef ALGORITHMIC_SKIP_LIST_H
 #define ALGORITHMIC_SKIP_LIST_H
+#include <iterator>
 
 
 namespace algic
 {
+    struct node_base;
+    
+    
+    /**********************************************************/
+    /*                     skip_list                          */
     template <class T>
     struct skip_list
     {
+        struct iterator
+            : public std::iterator<std::forward_iterator_tag, void, void, void, void>
+        {
+        };
+
+        skip_list();
+        ~skip_list();
+
+        bool insert(T const& t);
+        bool remove(T const& t);
+        bool contains(T const& t);
+        
+    private:
+        node_base*  mHead;
     };
 } // namespace algic
 
--- src/skip_list.hpp
+++ src/skip_list.hpp
@@ -9,16 +9,41 @@ namespace algic
 {
     using std::size_t;
 
-    template <class T>
+    template <class T, size_t _height>
+    struct node;
+
+
     struct node_base
     {
-        node_base(T const& t);
-        node_base(T&& t);
-        virtual ~node_base();
+        virtual ~node_base() {}
 
         virtual size_t height() const = 0;
         virtual node_base* next(size_t level) const = 0;
-        virtual void set(size_t level, node_base<T>* nb) = 0;
+        virtual void set(size_t level, node_base* nb) = 0;
+    };
+
+
+    template <size_t _height>
+    struct node_head : public node_base
+    {
+        node_head();
+        ~node_head();
+
+        size_t height() const override;
+        node_base* next(size_t level) const override;
+        void set(size_t level, node_base* nb) override;
+
+    private:
+        std::array<node_base*, _height>  mLinks;
+    };
+
+
+    template <class T, size_t _height>
+    struct node : public node_head<_height>
+    {
+        node(T const& t);
+        node(T&& t);
+        ~node();
 
         T& value();
         T const& value() const;
@@ -28,88 +53,98 @@ namespace algic
     };
 
 
-    template <class T, size_t _height>
-    struct node : public node_base<T>
-    {
-        node(T const& t);
-        node(T&& t);
-
-        size_t height() const override;
-        node_base<T>* next(size_t level) const override;
-        void set(size_t level, node_base<T>* nb) override;
-
-    private:
-        std::array<node_base<T>*, _height>  mLinks;
-    };
-
-
     /**********************************************************/
     /*                  implementations                       */
 
-    template <class T>
-    node_base<T>::node_base(T const& t)
-    {
-        new (static_cast<void*>(mData))T(t);
-    }
-
-    template <class T>
-    node_base<T>::node_base(T&& t)
-    {
-        new (static_cast<void*>(mData))T(std::move(t));
-    }
-
-    template <class T>
-    node_base<T>::~node_base()
-    {
-        static_cast<T*>(static_cast<void*>(mData))->~T();
-    }
-
-    template <class T>
-    T& node_base<T>::value()
-    {
-        return static_cast<T&>(*mData);
-    }
-
-    template <class T>
-    T const& node_base<T>::value() const
-    {
-        return static_cast<T const&>(*mData);
-    }
-
-
-
-    template <class T, size_t _height>
-    node<T, _height>::node(T const& t)
-        : node_base(t)
+    /**********************************************************/
+    /*                      node_head                         */
+    template <size_t _height>
+    node_head<_height>::node_head()
     {
         std::fill(std::begin(mLinks), std::end(mLinks), nullptr);
     }
 
-    template <class T, size_t _height>
-    node<T, _height>::node(T&& t)
-        : node_base(std::move(t))
+    template <size_t _height>
+    node_head<_height>::~node_head()
     {
-        std::fill(std::begin(mLinks), std::end(mLinks), nullptr);
     }
 
-    template <class T, size_t _height>
-    size_t node<T, _height>::height() const
+    template <size_t _height>
+    size_t node_head<_height>::height() const
     {
         return _height;
     }
 
-    template <class T, size_t _height>
-    node_base<T>* node<T, _height>::next(size_t level) const
+    template <size_t _height>
+    node_base* node_head<_height>::next(size_t level) const
     {
         assert(level < mLinks.size() && "Index out of range");
         return mLinks[level];
     }
 
-    template <class T, size_t _height>
-    void node<T, _height>::set(size_t level, node_base<T>* nb)
+    template <size_t _height>
+    void node_head<_height>::set(size_t level, node_base* nb)
     {
         assert(level < mLinks.size() && "Index out of range");
         mLinks[level] = nb;
+    }
+    
+
+    /**********************************************************/
+    /*                         node                           */
+    template <class T, size_t _height>
+    node<T, _height>::node(T const& t)
+    {
+        new (static_cast<void*>(mData))T(t);
+    }
+
+    template <class T, size_t _height>
+    node<T, _height>::node(T&& t)
+    {
+        new (static_cast<void*>(mData))T(std::move(t));
+    }
+
+    template <class T, size_t _height>
+    node<T, _height>::~node()
+    {
+        static_cast<T*>(static_cast<void*>(mData))->~T();
+    }
+
+    template <class T, size_t _height>
+    T& node<T, _height>::value()
+    {
+        return static_cast<T&>(*mData);
+    }
+
+    template <class T, size_t _height>
+    T const& node<T, _height>::value() const
+    {
+        return static_cast<T const&>(*mData);
+    }
+
+
+    
+    /**********************************************************/
+    /*                      skip_list                         */
+    template <class T>
+    skip_list<T>::skip_list()
+        : mHead(new node_head<1>())
+    {
+    }
+
+    template <class T>
+    bool skip_list<T>::insert(T const& t)
+    {
+    }
+
+    template <class T>
+    bool skip_list<T>::remove(T const& t)
+    {
+    }
+
+    template <class T>
+    bool skip_list<T>::contains(T const& t)
+    {
     }
 } // namespace algic
 
