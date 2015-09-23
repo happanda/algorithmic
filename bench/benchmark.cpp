facec4a19d3f5acc81d742250327788de5aaedaa
Pavel Moskalevich <happy.panda.code@gmail.com>
2015-09-23 16:44:28 +0300

    ascending/descending fill benchmarks

--- bench/benchmark.cpp
+++ bench/benchmark.cpp
@@ -9,28 +9,36 @@
 size_t const StrMaxLen = 30;
 
 std::vector<std::string>  sVect;
+std::vector<int>  sVectInts;
 random<int> sRand;
 using std::chrono::high_resolution_clock;
 
 
-void fillRandom(size_t count);
+void fillRandomStr(size_t count);
+void fillRandomInt(size_t count);
 void benchFill();
 void benchFillRange();
+void benchFillAsc();
+void benchFillDesc();
 
 
 /*********** MAIN ***********/
 int main(int argc, char* argv[])
 {
-    benchFill();
-    benchFillRange();
+    //benchFill();
+    //benchFillRange();
+    benchFillAsc();
+    benchFillDesc();
 
-
+    sVect.clear();
+    sVectInts.clear();
+    std::cout << "\n\nFINISHED" << std::endl;
     getchar();
     return 0;
 }
 
 
-void fillRandom(size_t count)
+void fillRandomStr(size_t count)
 {
     sVect.clear();
     for (size_t i = 0; i < count; ++i)
@@ -48,12 +56,22 @@ void fillRandom(size_t count)
     }
 }
 
+void fillRandomInt(size_t count)
+{
+    sVectInts.clear();
+    for (int i = 0; i < count; ++i)
+    {
+        sVectInts.push_back(i);
+    }
+}
+
 void benchFill()
 {
+    std::cout << "\n\nBENCH FILL" << std::endl;
     for (int c = 1; c <= 100000; c *= 10)
     {
         std::cout << std::endl << "Number of elements: " << c << std::endl;
-        fillRandom(c);
+        fillRandomStr(c);
         {
             std::set<std::string>  set;
             auto tpStart = high_resolution_clock::now();
@@ -64,6 +82,15 @@ void benchFill()
             auto tpFinish = high_resolution_clock::now();
             std::chrono::duration<double> diff = tpFinish - tpStart;
             std::cout << "  Set fill in " << diff.count() << std::endl;
+
+            tpStart = high_resolution_clock::now();
+            for (auto const& v : sVect)
+            {
+                set.erase(v);
+            }
+            tpFinish = high_resolution_clock::now();
+            diff = tpFinish - tpStart;
+            std::cout << "  Set erase in " << diff.count() << std::endl;
         }
 
         {
@@ -77,16 +104,26 @@ void benchFill()
             auto tpFinish = high_resolution_clock::now();
             std::chrono::duration<double> diff = tpFinish - tpStart;
             std::cout << "  SkipList fill in " << diff.count() << std::endl;
+
+            tpStart = high_resolution_clock::now();
+            for (auto const& v : sVect)
+            {
+                sl.erase(v);
+            }
+            tpFinish = high_resolution_clock::now();
+            diff = tpFinish - tpStart;
+            std::cout << "  SkipList erase in " << diff.count() << std::endl;
         }
     }
 }
 
 void benchFillRange()
 {
+    std::cout << "\n\nBENCH FILL RANGE" << std::endl;
     for (int c = 1; c <= 100000; c *= 10)
     {
         std::cout << std::endl << "Number of elements: " << c << std::endl;
-        fillRandom(c);
+        fillRandomStr(c);
         {
             std::set<std::string>  set;
             auto tpStart = high_resolution_clock::now();
@@ -94,6 +131,15 @@ void benchFillRange()
             auto tpFinish = high_resolution_clock::now();
             std::chrono::duration<double> diff = tpFinish - tpStart;
             std::cout << "  Set fill in " << diff.count() << std::endl;
+
+            tpStart = high_resolution_clock::now();
+            for (auto const& v : sVect)
+            {
+                set.erase(v);
+            }
+            tpFinish = high_resolution_clock::now();
+            diff = tpFinish - tpStart;
+            std::cout << "  Set erase in " << diff.count() << std::endl;
         }
 
         {
@@ -104,6 +150,119 @@ void benchFillRange()
             auto tpFinish = high_resolution_clock::now();
             std::chrono::duration<double> diff = tpFinish - tpStart;
             std::cout << "  SkipList fill in " << diff.count() << std::endl;
+
+            tpStart = high_resolution_clock::now();
+            for (auto const& v : sVect)
+            {
+                sl.erase(v);
+            }
+            tpFinish = high_resolution_clock::now();
+            diff = tpFinish - tpStart;
+            std::cout << "  SkipList erase in " << diff.count() << std::endl;
+        }
+    }
+}
+
+void benchFillAsc()
+{
+    std::cout << "\n\nBENCH FILL ASCENDING" << std::endl;
+    for (int c = 1; c <= 100000; c *= 10)
+    {
+        std::cout << std::endl << "Number of elements: " << c << std::endl;
+        fillRandomInt(c);
+        {
+            std::set<int>  set;
+            auto tpStart = high_resolution_clock::now();
+            for (auto const& v : sVectInts)
+            {
+                set.insert(v);
+            }
+            auto tpFinish = high_resolution_clock::now();
+            std::chrono::duration<double> diff = tpFinish - tpStart;
+            std::cout << "  Set fill in " << diff.count() << std::endl;
+
+            tpStart = high_resolution_clock::now();
+            for (auto const& v : sVectInts)
+            {
+                set.erase(v);
+            }
+            tpFinish = high_resolution_clock::now();
+            diff = tpFinish - tpStart;
+            std::cout << "  Set erase in " << diff.count() << std::endl;
+        }
+
+        {
+            random<float> floatRand;
+            algic::skip_list<int, random<float>>  sl(floatRand);
+            auto tpStart = high_resolution_clock::now();
+            for (auto const& v : sVectInts)
+            {
+                sl.insert(v);
+            }
+            auto tpFinish = high_resolution_clock::now();
+            std::chrono::duration<double> diff = tpFinish - tpStart;
+            std::cout << "  SkipList fill in " << diff.count() << std::endl;
+
+            tpStart = high_resolution_clock::now();
+            for (auto const& v : sVectInts)
+            {
+                sl.erase(v);
+            }
+            tpFinish = high_resolution_clock::now();
+            diff = tpFinish - tpStart;
+            std::cout << "  SkipList erase in " << diff.count() << std::endl;
+        }
+    }
+}
+
+void benchFillDesc()
+{
+    std::cout << "\n\nBENCH FILL DESCENDING" << std::endl;
+    for (int c = 1; c <= 100000; c *= 10)
+    {
+        std::cout << std::endl << "Number of elements: " << c << std::endl;
+        fillRandomInt(c);
+        {
+            std::set<int>  set;
+            auto tpStart = high_resolution_clock::now();
+            for (auto v = std::crbegin(sVectInts); v != std::crend(sVectInts); ++v)
+            {
+                set.insert(*v);
+            }
+            auto tpFinish = high_resolution_clock::now();
+            std::chrono::duration<double> diff = tpFinish - tpStart;
+            std::cout << "  Set fill in " << diff.count() << std::endl;
+
+            tpStart = high_resolution_clock::now();
+            for (auto v = std::crbegin(sVectInts); v != std::crend(sVectInts); ++v)
+            {
+                set.erase(*v);
+            }
+            tpFinish = high_resolution_clock::now();
+            diff = tpFinish - tpStart;
+            std::cout << "  Set erase in " << diff.count() << std::endl;
+        }
+
+        {
+            random<float> floatRand;
+            algic::skip_list<int, random<float>>  sl(floatRand);
+            auto tpStart = high_resolution_clock::now();
+            for (auto v = std::crbegin(sVectInts); v != std::crend(sVectInts); ++v)
+            {
+                sl.insert(*v);
+            }
+            auto tpFinish = high_resolution_clock::now();
+            std::chrono::duration<double> diff = tpFinish - tpStart;
+            std::cout << "  SkipList fill in " << diff.count() << std::endl;
+
+            tpStart = high_resolution_clock::now();
+            for (auto v = std::crbegin(sVectInts); v != std::crend(sVectInts); ++v)
+            {
+                sl.erase(*v);
+            }
+            tpFinish = high_resolution_clock::now();
+            diff = tpFinish - tpStart;
+            std::cout << "  SkipList erase in " << diff.count() << std::endl;
         }
     }
 }
